3f874d5db1df784c01021a2ebfef6e8397350a76
Benjamin Weißenfels <b.pixeldrama@gmail.com>
2012-05-25 11:43:57 +0200

    untabify

--- Test/Canvas.hs
+++ Test/Canvas.hs
@@ -7,11 +7,9 @@ import Test.HUnit
 import Graphics.UI.Gtk
 import Graphics.Rendering.Cairo
 
-drawTest = TCanvas.draw drawBlack 
+drawTest = TCanvas.draw drawBlack
 
 drawBlack :: (Point -> Color)
-drawBlack (a, b)  
-  | a == b = Color 0 0 0                          
+drawBlack (a, b)
+  | a == b = Color 0 0 0
   | otherwise = Color 65535 65535 65535
-
-
